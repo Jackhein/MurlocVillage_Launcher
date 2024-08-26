fb7e43863b5b324fc091d4c9663d9d113ed94968
jackhein <moustiquejunior@gmail.com>
2024-08-26 21:14:55 +0200

    Re-enable download. Return only after error or when each file has been checked. Display hash result.

--- src/file_integrity.rs
+++ src/file_integrity.rs
@@ -32,13 +32,11 @@ mod qobject {
 
     impl qobject::FileIntegrity {
         #[qinvokable]
-        pub fn check_file(mut self: Pin<&mut Self>) {
-            //use qobject::FileIntegrity::{display_message, download_file_process};
+        pub fn check_file(self: Pin<&mut Self>) {
             let qt_thread = self.qt_thread();
 
             thread::spawn(move || {
-                //let mut locked_self = thread_this.lock().unwrap();
-                //locked_self.as_mut().set_result(QString::from("test"));
+                qobject::FileIntegrity::display_message(&qt_thread, "Begin check_file.");
                 let runtime = tokio::runtime::Runtime::new().unwrap();
                 let current_path = env::current_dir().unwrap().display().to_string();
                 let data = match fs::read_to_string("resources/hashmap.json") {
@@ -58,10 +56,10 @@ mod qobject {
                                 let current_file = format!("{}/{}", current_path, file);
 
                                 if !Path::new(&current_file).exists() {
-                                    // if runtime.block_on(self.download_file_process(url, &current_file)) {
-                                    //     // qobject::FileIntegrity::display_message(qt_thread, &format!("Unable to download {} from {}.", file, url));
-                                    //     return
-                                    // }
+                                    if runtime.block_on(qobject::FileIntegrity::download_file_process(url, &current_file)) {
+                                        qobject::FileIntegrity::display_message(&qt_thread, &format!("Unable to download {} from {}.", file, url));
+                                        return
+                                    }
                                     qobject::FileIntegrity::display_message(&qt_thread, &format!("File {} didn't exist, downloaded from {}.", file, url));
                                 }
                                 let mut hasher = Sha512::new();
@@ -74,27 +72,28 @@ mod qobject {
                                 };
 
                                 let _ = io::copy(&mut file_io, &mut hasher);
-                                if hex::encode(hasher.finalize()) == correct_hash {
+                                if hex::encode(hasher.clone().finalize()) == correct_hash {
                                     qobject::FileIntegrity::display_message(&qt_thread, &format!("File {} is correct.", file));
                                 } else {
                                     if fs::remove_file(&current_file).is_err() {
                                         qobject::FileIntegrity::display_message(&qt_thread, &format!("Unable to delete {}.", file));
-                                        return;
-                                //     } else if runtime.block_on(self.download_file_process(url, &current_file)) {
-                                //         // qobject::FileIntegrity::display_message(qt_thread, &format!("Unable to download {} from {}.", file, url));
-                                //         return;
+                                        return
+                                    } else if runtime.block_on(qobject::FileIntegrity::download_file_process(url, &current_file)) {
+                                        qobject::FileIntegrity::display_message(&qt_thread, &format!("Unable to download {} from {}.", file, url));
+                                        return
                                     }
-                                    qobject::FileIntegrity::display_message(&qt_thread, &format!("File {} isn't correct, download from {}.", file, url));
-                                    return;
+                                    qobject::FileIntegrity::display_message(&qt_thread, &format!("File {} isn't correct ({}), download from {}.", file, hex::encode(&hasher.finalize()), url));
                                 }
                             } else {
                                 qobject::FileIntegrity::display_message(&qt_thread, &format!("File {} data in JSON are corrupted (wrong array size)", file));
-                                return;
+                                return
                             }
                         } else {
                             qobject::FileIntegrity::display_message(&qt_thread, &format!("File {} data in JSON are corrupted (not an array)", file));
                         }
                     }
+                    qobject::FileIntegrity::display_message(&qt_thread, "Game is ready");
+                    return
                 }
                 qobject::FileIntegrity::display_message(&qt_thread, "JSON isn't valid");
             });
@@ -103,11 +102,11 @@ mod qobject {
         fn display_message(qt_thread: &UniquePtr<FileIntegrityCxxQtThread>, msg: &str) {
             println!("{}", msg);
             let queued_msg = Arc::new(msg.to_string());
-            qt_thread.queue(move |mut qobject| {
+            qt_thread.queue(move | qobject| {
                 qobject.set_result(QString::from(queued_msg.as_str()));
             }).unwrap();
         }
-        async fn download_file(&self, url: &str, current_file: String) -> Result<(), String> {
+        async fn download_file(url: &str, current_file: String) -> Result<(), String> {
             let response = reqwest::get(url).await.map_err(|_| "DownloadFailure")?;
             if !response.status().is_success() {
                 //Err(reqwest::Error::new(reqwest::StatusCode::from(response.status()), "Failed to download file"));
@@ -121,13 +120,13 @@ mod qobject {
             Ok(())
         }
 
-        async fn download_file_process(&self, url: &str, current_file: &String) -> bool {
+        async fn download_file_process(url: &str, current_file: &String) -> bool {
             let mut retry = 0;
             let max_retries = 3;
 
             while retry < max_retries {
                 retry += 1;
-                match self.download_file(url, current_file.clone()).await {
+                match qobject::FileIntegrity::download_file(url, current_file.clone()).await {
                     Ok(_) => {
                         println!("File downloaded successfully!");
                         return false;
